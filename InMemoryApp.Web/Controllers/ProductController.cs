482fed71084440e1196e8d70603afd54d56b9bb3
Ali Bulut <ali@alibulut.net>
2021-08-05 16:00:27 +0300

    AbsoluteExpiration and SlidingExpiration properties

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -20,24 +20,29 @@ namespace InMemoryApp.Web.Controllers
         public IActionResult Index()
         {
             // first way to check if time is in memory
-            if (string.IsNullOrEmpty(_memoryCache.Get<string>("time"))){
-                _memoryCache.Set<string>("time", DateTime.Now.ToString());
-            }
+            //if (string.IsNullOrEmpty(_memoryCache.Get<string>("time"))){
+            //    _memoryCache.Set<string>("time", DateTime.Now.ToString());
+            //}
 
             // second way
-            if(!(_memoryCache.TryGetValue<string>("time", out string timeCache)))
+            // if there is already time in memory, timeCache is the value of time.
+            if (!(_memoryCache.TryGetValue<string>("time", out string timeCache)))
             {
-                // if there is already time in memory, timeCache is the value of time.
-                _memoryCache.Set<string>("time", DateTime.Now.ToString());
+                // setting cache properties
+                MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+                options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+                options.SlidingExpiration = TimeSpan.FromSeconds(10);
+
+                _memoryCache.Set<string>("time", DateTime.Now.ToString(), options);
             }
 
             // third way => if there is already time in memory, it will return the time, if there is not, it will set
             // time by using the second parameter and return it.
-            _memoryCache.GetOrCreate<string>("time", entry =>
-            {
-                // by using entry we can set the cache properties such as lifespan or etc.
-                return DateTime.Now.ToString();
-            });
+            //_memoryCache.GetOrCreate<string>("time", entry =>
+            //{
+            //    // by using entry we can set the cache properties such as lifespan or etc.
+            //    return DateTime.Now.ToString();
+            //});
 
             // it will remove time from the memory.
             //_memoryCache.Remove("time");
@@ -47,7 +52,10 @@ namespace InMemoryApp.Web.Controllers
 
         public IActionResult Show()
         {
-            ViewBag.time = _memoryCache.Get<string>("time");
+            _memoryCache.TryGetValue<string>("time", out string timeCache);
+            //ViewBag.time = _memoryCache.Get<string>("time");
+            ViewBag.time = timeCache;
+
             return View();
         }
     }
