57743660cd3dd6fe3ceccb51b1516703b455a36a
Ali Bulut <ali@alibulut.net>
2021-08-06 15:49:41 +0300

    caching complex types

--- IDistributedCacheRedisApp.Web/Controllers/ProductController.cs
+++ IDistributedCacheRedisApp.Web/Controllers/ProductController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using IDistributedCacheRedisApp.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
 
@@ -24,20 +27,53 @@ namespace IDistributedCacheRedisApp.Web.Controllers
             _distributedCache.SetString("name", "Ali", cacheEntryOptions);
             await _distributedCache.SetStringAsync("surname", "Bulut", cacheEntryOptions);
 
+            Product p = new Product() { Id = 1, Name = "pen", Price = 10 };
+
+            // serialize method (recommended)
+            string jsonProduct = JsonSerializer.Serialize(p);
+            await _distributedCache.SetStringAsync("product:1", jsonProduct, cacheEntryOptions);
+
+            // binary method
+            Byte[] byteProduct = Encoding.UTF8.GetBytes(jsonProduct);
+            _distributedCache.Set("product:1:byte", byteProduct, cacheEntryOptions);
+
             return View();
         }
 
-        public IActionResult Show()
+        public async Task<IActionResult> ShowAsync()
         {
             string name = _distributedCache.GetString("name");
             ViewBag.name = name;
 
+            string surname = await _distributedCache.GetStringAsync("surname");
+            ViewBag.surname = surname;
+
+            // deserialize method (recommended)
+            string jsonProduct = _distributedCache.GetString("product:1");
+            if (!string.IsNullOrEmpty(jsonProduct))
+            {
+                Product p = JsonSerializer.Deserialize<Product>(jsonProduct);
+                ViewBag.product = p;
+            }
+            
+            // binary method
+            Byte[] byteProduct = _distributedCache.Get("product:1:byte");
+            if (byteProduct != null)
+            {
+                string stringProduct = Encoding.UTF8.GetString(byteProduct);
+                Product p2 = JsonSerializer.Deserialize<Product>(stringProduct);
+                ViewBag.product2 = p2;
+            }
+
             return View();
         }
 
-        public IActionResult Remove()
+        public async Task<IActionResult> RemoveAsync()
         {
             _distributedCache.Remove("name");
+            await _distributedCache.RemoveAsync("surname");
+            _distributedCache.Remove("product:1");
+            _distributedCache.Remove("product:1:byte");
 
             return View();
         }
