692be672c9aede7c8f69e79b8800504b3a1607db
Ali Bulut <ali@alibulut.net>
2021-08-06 14:47:25 +0300

    set, get and remove methods

--- IDistributedCacheRedisApp.Web/Controllers/ProductController.cs
+++ IDistributedCacheRedisApp.Web/Controllers/ProductController.cs
@@ -16,8 +16,29 @@ namespace IDistributedCacheRedisApp.Web.Controllers
             _distributedCache = distributedCache;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
+            cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+
+            _distributedCache.SetString("name", "Ali", cacheEntryOptions);
+            await _distributedCache.SetStringAsync("surname", "Bulut", cacheEntryOptions);
+
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            string name = _distributedCache.GetString("name");
+            ViewBag.name = name;
+
+            return View();
+        }
+
+        public IActionResult Remove()
+        {
+            _distributedCache.Remove("name");
+
             return View();
         }
     }
