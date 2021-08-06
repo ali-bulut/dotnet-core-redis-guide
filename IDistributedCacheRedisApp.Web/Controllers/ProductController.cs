6803520447580f6833e027518552998401327ac6
Ali Bulut <ali@alibulut.net>
2021-08-06 14:38:53 +0300

    creating project and setup redis

---
+++ IDistributedCacheRedisApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace IDistributedCacheRedisApp.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IDistributedCache _distributedCache;
+
+        public ProductController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
